55225db01f25e8e2c311eca5ad93b71094e7ca9c
Didier Iradukunda <didier.iradukunda01@gmail.com>
2022-04-27 23:51:42 +0200

    drop duplicate modified

--- clean_tweets_dataframe.py
+++ clean_tweets_dataframe.py
@@ -8,6 +8,7 @@ class Clean_Tweets:
         self.df = df
         print('Automation in Action...!!!')
         
+        
     def drop_unwanted_column(self, df:pd.DataFrame)->pd.DataFrame:
         """
         remove rows that has column names. This error originated from
@@ -16,16 +17,17 @@ class Clean_Tweets:
         unwanted_rows = df[df['retweet_count'] == 'retweet_count' ].index
         df.drop(unwanted_rows , inplace=True)
         df = df[df['polarity'] != 'polarity']
-        
         return df
+    
+    
     def drop_duplicate(self, df:pd.DataFrame)->pd.DataFrame:
         """
         drop duplicate rows
         """
-        
-        ---
-        
+        self.df = self.df.drop_duplicates().drop_duplicates(subset='original_text')
         return df
+    
+    
     def convert_to_datetime(self, df:pd.DataFrame)->pd.DataFrame:
         """
         convert column to datetime
